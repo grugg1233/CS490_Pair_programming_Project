27725fb0d071c3ec66c1c32382261a7a241068fd
Rishik <rishikreddy.yesgari@gmail.com>
2026-01-30 00:51:42 -0500

    Working Navbar and hero

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import Hero from "./Components/Hero"
+import Navbar from "./Components/Navbar"
 
 export default function App() {
   return (
   <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Navbar></Navbar>
+    <Hero></Hero>
   </>
   )
 }
