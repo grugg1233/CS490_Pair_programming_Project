95cf6abd27e402b3dad680202f1690581301ec3b
grugg1233 <gma35@njit.edu>
2026-01-30 01:02:50 -0500

    Hero component updated

--- frontend/src/Components/Hero.tsx
+++ frontend/src/Components/Hero.tsx
@@ -1,21 +1,25 @@
 const Hero = () => {
     return (
-    <div className="hero bg-base-200 min-h-screen">
-    <div className="hero-content flex-col lg:flex-row">
-        <img
-        src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
-        className="max-w-sm rounded-lg shadow-2xl"
-        />
-        <div>
-        <h1 className="text-5xl font-bold">Box Office News!</h1>
-        <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-            quasi. In deleniti eaque aut repudiandae et a id nisi.
-        </p>
-        <button className="btn btn-primary">Get Started</button>
+        <div
+        className="hero min-h-screen"
+        style={{
+            backgroundImage:
+            "url(https://assets.westchestermagazine.com/wp-content/uploads/2023/10/movie-theater-adobe-stock.jpg)",
+        }}
+        >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center">
+            <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+            </div>
         </div>
-    </div>
-    </div>
+        </div>
     )
 }
 export default Hero; 
+
