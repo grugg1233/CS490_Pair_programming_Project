ffeec0b05fee35b162d03760e7cf5eb53a7655c2
Rishik <rishikreddy.yesgari@gmail.com>
2026-02-03 17:08:10 -0500

    Change navbar to be sticky and add icon

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,17 +1,16 @@
+import icon from "../assets/icon.png";
+
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100 shadow-sm">
-      <div className="flex-1">
-        <a className="btn btn-ghost text-xl">SAKILA.</a>
+    <div className="navbar bg-black shadow-sm sticky top-0 z-50 border-b-[0.5px]">
+      <div className="flex-1 items-center">
+        <img src={icon} alt="Icon description" className="h-16 w-16"></img>
+        <a className="btn btn-ghost text-[32px]">R&G.</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-4 space-x-4">
-          <li>
-            Home
-          </li>
-          <li>
-            Films
-          </li>
+          <li className="text-[16px]">Home</li>
+          <li className="text-[16px]">Films</li>
         </ul>
       </div>
     </div>
