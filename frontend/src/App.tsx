ca820884542640fc5b26974df7ca8e7e38f9ef61
grugg1233 <gma35@njit.edu>
2026-02-03 13:29:34 -0500

    top film dropdowns for additional infomration, top 5 actors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import Hero from "./Components/Hero"
 import Navbar from "./Components/Navbar"
 import HomeFilm from "./Components/HomeFilm"
+import TopActors from "./Components/TopActors"
+
 export default function App() {
   return (
   <>
     <Navbar></Navbar>
     <Hero></Hero>
     <HomeFilm></HomeFilm>
+    <TopActors></TopActors>
   </>
   )
 }
--- frontend/src/Components/HomeFilm.tsx
+++ frontend/src/Components/HomeFilm.tsx
@@ -3,7 +3,7 @@ const HomeFilm = () => {
     return (
         <section className="bg-black">
             <h1 className="text-3xl font-bold leading-tight text-white p-3  ">Top Rented Films</h1>
-        <StyledButton rank={1} title="Movie Name" rentals={500} />
+        <StyledButton rank={1} title="Movie Name" rentals={500} imageU="https://static.vecteezy.com/system/resources/thumbnails/072/460/715/small/film-strip-capturing-city-street-at-nightgraphy-concept-photo.jpg" />
          </section>
     );
 }
