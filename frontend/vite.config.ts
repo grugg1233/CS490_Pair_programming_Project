2d2bd0551ed8f2401807cadf2a1e0472f37dd40f
Rishik <rishikreddy.yesgari@gmail.com>
2026-02-03 08:07:19 -0500

    Finished frontend dockerization

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+//Had to add server -> host:true for docker compose to work https://forums.docker.com/t/dockerize-a-react-vite-application/140270/2 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
