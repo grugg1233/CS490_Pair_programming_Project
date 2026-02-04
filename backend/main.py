687a10c4964bccca0ac366978ea4250623ec19f1
Rishik <rishikreddy.yesgari@gmail.com>
2026-02-04 15:01:32 -0500

    Added api for films

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
+from test import return_films
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def return_films():
+    films = return_films()
+    return films
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
--- backend/test.py
+++ backend/test.py
@@ -3,36 +3,35 @@
 import mysql.connector as connecter
 
 
-connection = connecter.connect(
-    user="root", password="root", host="localhost", port="3306", database="sakila"
-)
+def return_films():
+    connection = connecter.connect(
+        user="root", password="root", host="localhost", port="3306", database="sakila"
+    )
+    cursor = connection.cursor()
 
-cursor = connection.cursor()
+    cursor.execute(
+        """SELECT
+        film.film_id,
+        film.title,
+        COUNT(rental.rental_id) AS rental_count
+    FROM rental
+    JOIN inventory ON rental.inventory_id = inventory.inventory_id
+    JOIN film ON inventory.film_id = film.film_id
+    JOIN film_actor ON film.film_id = film_actor.film_id
+    WHERE film_actor.actor_id = (
+        SELECT film_actor.actor_id
+        FROM film_actor
+        GROUP BY film_actor.actor_id
+        ORDER BY COUNT(film_actor.film_id) DESC
+        LIMIT 1
+    )
+    GROUP BY film.film_id, film.title
+    ORDER BY rental_count DESC
+    LIMIT 5;"""
+    )
 
-cursor.execute(
-    """SELECT
-    film.film_id,
-    film.title,
-    COUNT(rental.rental_id) AS rental_count
-FROM rental
-JOIN inventory ON rental.inventory_id = inventory.inventory_id
-JOIN film ON inventory.film_id = film.film_id
-JOIN film_actor ON film.film_id = film_actor.film_id
-WHERE film_actor.actor_id = (
-    SELECT film_actor.actor_id
-    FROM film_actor
-    GROUP BY film_actor.actor_id
-    ORDER BY COUNT(film_actor.film_id) DESC
-    LIMIT 1
-)
-GROUP BY film.film_id, film.title
-ORDER BY rental_count DESC
-LIMIT 5;"""
-)
+    films = cursor.fetchall()
 
-films = cursor.fetchall()
+    connection.close()
 
-connection.close()
-
-for film in films:
-    print(film, "\n")
+    return films
