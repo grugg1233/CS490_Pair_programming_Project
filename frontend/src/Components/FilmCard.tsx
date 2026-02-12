245ff0bc6ed77289a4cff291eb394d2d79969a7b
Rishik <rishikreddy.yesgari@gmail.com>
2026-02-11 19:31:38 -0500

    Change type

--- frontend/src/Components/FilmCard.tsx
+++ frontend/src/Components/FilmCard.tsx
@@ -3,10 +3,10 @@ import type { FilmData } from "../utils/types";
 interface FilmCardProps {
   film: FilmData;
   onClick: (filmId: number | null) => void;
-  showFilmId : boolean;
+  showFilmId: boolean;
 }
 
-const Card = ({ film, onClick, showFilmId}: FilmCardProps) => {
+const Card = ({ film, onClick, showFilmId }: FilmCardProps) => {
   return (
     <div className="justify-center flex w-full md:block md:w-auto lg:block lg:w-auto">
       <div
@@ -17,15 +17,16 @@ const Card = ({ film, onClick, showFilmId}: FilmCardProps) => {
             ring-1 ring-white/20"
       >
         <div className="card-body">
-          { showFilmId &&
-          <div
-            className="absolute top-3 left-3 
+          {showFilmId && (
+            <div
+              className="absolute top-3 left-3 
             flex h-10 w-10 items-center justify-center
             rounded-full bg-red-600
             text-[18px] font-bold text-white"
-          >
-            {film.film_id}
-          </div>}
+            >
+              {film.film_id}
+            </div>
+          )}
           <div className="pr-2 absolute bottom-3 left-3 text-left">
             <h2 className="card-title font-bold text-[24px]">{film.title}</h2>
             {film.category && <h2>Category: {film.category}</h2>}
